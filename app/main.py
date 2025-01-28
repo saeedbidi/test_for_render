0bded704a377cf46e9436cc226f3f072126fe441
Saeed <102333571+saeedbidi@users.noreply.github.com>
2025-01-28 11:52:45 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,4 +7,5 @@ def home():
     return "Hello, Render!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
